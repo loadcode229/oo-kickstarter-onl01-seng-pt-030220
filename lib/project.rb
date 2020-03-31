effd699ddf549f4fbc0fc651691617225548555f
Adam Priest <sepone229@gmail.com>
2020-03-31 10:12:55 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,9 @@
 class Project
-  
   attr_reader :backers, :title
-  
   def initialize(title)
     @title = title
     @backers = []
   end
-  
   def add_backer(backer)
     @backers << backer
     backer.backed_projects << self
